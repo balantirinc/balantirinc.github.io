86c70a449a8d605ef4d27f05387fc1c652047862
yureru <bryan.risc@gmail.com>
2024-03-23 23:26:28 -0600

    [bal-0000]: Set up project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-7xl'>Balantir</h1>
+      <p className='pt-6'>Coming soon...</p>
+    </div>
+  )
+}
+
+export default App
