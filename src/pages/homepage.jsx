663f46ca19dfeead99d4aaaad9a1b95a5a337d99
yureru <bryan.risc@gmail.com>
2024-04-04 00:50:10 -0600

    [bal-0017]: Adds hero component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,9 +1,11 @@
 import Footer from '../components/footer'
+import HeroDiagonal from '../components/hero-diagonal'
 import TrustedBy from './../components/trusted-by'
 
 export function HomePage() {
   return (
     <>
+      <HeroDiagonal />
       <h1 className="text-7xl">Homepage</h1>
       <TrustedBy />
       <Footer />
