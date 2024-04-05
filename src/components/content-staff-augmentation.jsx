acfa758a7dd99d06308a5e2ee9232fcccbcc2dc2
yureru <bryan.risc@gmail.com>
2024-04-05 01:12:48 -0600

    [bal-0020]: Adds staff augmentation section

---
+++ src/components/content-staff-augmentation.jsx
@@ -0,0 +1,77 @@
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  LockClosedIcon,
+} from '@heroicons/react/20/solid'
+
+const features = [
+  {
+    name: 'Know your staffing needs',
+    description:
+      'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.',
+    href: '#',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'Selecting the best talent',
+    description:
+      'Not only technical but also culturally. We ensure that you get the best team members for your open positions.',
+    href: '#', // link: How we achieve this?
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues',
+    description:
+      'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.',
+    href: '#',
+    icon: ArrowPathIcon,
+  },
+]
+
+export default function ContentStaffAugmentation() {
+  return (
+    <div className="bg-gray-900 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-400">
+            Build your team
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Looking to augment your staff?
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            From one dev, up to an entire team we provide everything to augment
+            your staff with <strong>top %1 engineers</strong> from around the
+            globe.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+            {features.map((feature) => (
+              <div key={feature.name} className="flex flex-col">
+                <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-white">
+                  <feature.icon
+                    className="h-5 w-5 flex-none text-indigo-400"
+                    aria-hidden="true"
+                  />
+                  {feature.name}
+                </dt>
+                <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-300">
+                  <p className="flex-auto">{feature.description}</p>
+                  <p className="mt-6">
+                    <a
+                      href={feature.href}
+                      className="text-sm font-semibold leading-6 text-indigo-400"
+                    >
+                      Learn more <span aria-hidden="true">→</span>
+                    </a>
+                  </p>
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,5 +1,6 @@
 import ContentProductSticky from '../components/content-product-sticky'
 import ContentProductProcess from '../components/content-product-process'
+import ContentStaffAugmentation from '../components/content-staff-augmentation'
 import Footer from '../components/footer'
 import HeroDiagonal from '../components/hero-diagonal'
 import TrustedBy from './../components/trusted-by'
@@ -10,6 +11,7 @@ export function HomePage() {
       <HeroDiagonal />
       {/* <ContentProductSticky /> */}
       <ContentProductProcess />
+      <ContentStaffAugmentation />
       <TrustedBy />
       <Footer />
     </>
