1cdb72f523045ef66f798171405ca4b2f50281fa
yureru <bryan.risc@gmail.com>
2024-04-04 23:55:49 -0600

    [bal-0018]: Adds sticky product info and testimonial

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,3 +1,5 @@
+import ContentProductSticky from '../components/content-product-sticky'
+import ContentProductTestimonial from '../components/content-product-testimonial'
 import Footer from '../components/footer'
 import HeroDiagonal from '../components/hero-diagonal'
 import TrustedBy from './../components/trusted-by'
@@ -7,6 +9,8 @@ export function HomePage() {
     <>
       <HeroDiagonal />
       <h1 className="text-7xl">Homepage</h1>
+      <ContentProductSticky />
+      <ContentProductTestimonial />
       <TrustedBy />
       <Footer />
     </>
