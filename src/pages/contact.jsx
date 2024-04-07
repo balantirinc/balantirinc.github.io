e07cd83e8b75d38d420f5f7ee7308fd68e400d2c
yureru <bryan.risc@gmail.com>
2024-04-07 02:00:32 -0600

    [bal-0026]: Contact form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,12 @@
 import Footer from '../components/footer'
 import NavMenuWithoutHero from '../components/nav-menu-without-hero'
+import ContactFormSplit from '../components/contact-form-split'
 
 export function Contact() {
   return (
     <>
       <NavMenuWithoutHero />
-
+      <ContactFormSplit />
       <Footer />
     </>
   )
